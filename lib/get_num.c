18bb3dd47dfc4fd38ef225959adee82d595941fd
dgeene <david.geene@gmail.com>
2023-09-29 20:20:44 -0400

    add get_num and error functions

---
+++ lib/get_num.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <errno.h>
+#include "get_num.h"
+
+static void gnFail(const char *fname, const char *msg, const char *arg, const char *name) {
+
+}
+
+static long getNum(const char *fname, const char *arg, int flags, const char *name) {
+
+}
+
+long getLong(const char *arg, int flags, const char *name) {
+
+}
+
+int getInt(const char *arg, int flags, const char *name) {
+    
+}
