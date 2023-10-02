b423271be2290c7c7ee88d84d85e0c398e25af14
dgeene <david.geene@gmail.com>
2023-10-02 13:20:27 -0400

    add fileio

---
+++ fileio/copy.c
@@ -0,0 +1,11 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "tlpi_hdr.h"
+
+#ifndef BUF_SIZE // allow "cc -D" to override definition
+#define BUF_SIZE 1024
+#endif
+
+int main() {
+    return 0;
+}
